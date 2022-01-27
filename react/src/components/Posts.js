b01055b2036c07d9385e7e2ad55254e9d5591800
niolha <olhanikulina2@gmail.com>
2022-01-28 00:23:41 +0100

    remove react

--- react/src/components/Posts.js
+++
@@ -1,9 +0,0 @@
-export default function Posts(props) {
-  return (
-    <div className="grid-items">
-      <h4 className="title"> {props.title}</h4>
-      <p className="content">{props.description}</p>
-      <button className="dark"> click</button>
-    </div>
-  );
-}
